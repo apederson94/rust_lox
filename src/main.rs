27be9448139fee2ba50be2d3ed5ee5002e252bef
Austin Pederson <austinpederson@pm.me>
2025-05-19 21:37:42 -0700

    code cleanup for better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::env::args;
 
 mod errors;
-mod run;
-mod run_file;
-mod run_prompt;
+mod runner;
 mod scanner;
 mod token;
 
@@ -14,8 +12,14 @@ fn main() {
     if arguments.len() > 2 {
         std::process::exit(64);
     } else if arguments.len() == 2 {
-        run_file::run_file(arguments[1].clone())
+        match runner::run_file(arguments[1].clone()) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("{}", e);
+                std::process::exit(99);
+            }
+        }
     } else {
-        run_prompt::run_prompt()
+        runner::run_prompt()
     }
 }
---
+++ src/runner.rs
@@ -0,0 +1,56 @@
+use std::{
+    fmt::Display,
+    fs,
+    io::{self, Write},
+    process,
+};
+
+use crate::{errors, scanner};
+
+pub fn run_file(path: String) -> Result<(), RunnerError> {
+    let data = fs::read_to_string(path);
+
+    match data {
+        Ok(script) => Ok(run(script)),
+        Err(_) => Err(RunnerError::FailedToRunFile),
+    }
+}
+
+pub fn run_prompt() {
+    println!("Starting REPL...");
+    let stdin = io::stdin();
+    let input = &mut String::new();
+
+    loop {
+        print!("> ");
+        let _ = io::stdout().flush();
+        match stdin.read_line(input) {
+            Ok(_) => run(input.clone()),
+            Err(_) => break,
+        }
+        input.clear();
+    }
+}
+
+fn run(script: String) {
+    let mut scanner = scanner::Scanner::new(script);
+    let tokens = scanner.scan_tokens();
+
+    if errors::had_error() {
+        process::exit(65);
+    }
+
+    tokens.iter().for_each(|t| println!("{:?}", t));
+}
+
+pub enum RunnerError {
+    FailedToRunFile,
+}
+
+impl Display for RunnerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::FailedToRunFile => write!(f, "Failed to run file!"),
+        }
+    }
+}
