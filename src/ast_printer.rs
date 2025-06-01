9017ac813f798b22b49e75c55deb737664088c00
Austin Pederson <austinpederson@pm.me>
2025-06-01 13:16:23 -0700

    start work on AST

---
+++ src/ast_printer.rs
@@ -0,0 +1,28 @@
+use crate::expr::Expr;
+use crate::token::TokenType;
+
+trait AstPrinter {
+    fn print(&self) -> String;
+}
+
+impl AstPrinter for Expr {
+    fn print(&self) -> String {
+        match self {
+            Expr::Binary {
+                left,
+                operator,
+                right,
+            } => {
+                format!("({} {} {})", operator.lexeme(), left.print(), right.print())
+            }
+            Expr::Grouping { expression } => format!("(group {})", expression.print()),
+            Expr::Literal { value } => match value.which() {
+                TokenType::Number(num) => format!("{}", num),
+                TokenType::Str(text) => format!("\"{}\"", text),
+                TokenType::Identifier(ident) => format!("{}", ident),
+                _ => format!("Error: Unknown literal type!"),
+            },
+            Expr::Unary { operator, right } => format!("({} {})", operator.lexeme(), right.print()),
+        }
+    }
+}
