d70bb4980020c94ee0cc0c9826ab1a3d4ce24d3e
Austin Pederson <austinpederson@pm.me>
2025-07-06 14:21:00 -0700

    add more sepcific interpretation errors

--- src/interpretable.rs
+++ src/interpretable.rs
@@ -1,4 +1,10 @@
-use crate::{expr::Expr, lox_value::LoxValue, token::TokenType};
+use std::fmt::Display;
+
+use crate::{
+    expr::Expr,
+    lox_value::LoxValue,
+    token::{Token, TokenType},
+};
 
 pub trait Interpretable {
     fn interpret(&self) -> Result<LoxValue, InterpretationError>;
@@ -13,7 +19,7 @@ impl Interpretable for Expr {
                 TokenType::True => Ok(LoxValue::Bool(true)),
                 TokenType::False => Ok(LoxValue::Bool(false)),
                 TokenType::EndOfFile => Ok(LoxValue::Nil),
-                _ => Err(InterpretationError::new(self)),
+                _ => Err(InterpretationError::InvalidLiteral(value.clone())),
             },
             Expr::Grouping { expression } => expression.interpret(),
             Expr::Unary { operator, right } => {
@@ -23,7 +29,7 @@ impl Interpretable for Expr {
                         if let LoxValue::Number(n) = right_value {
                             Ok(LoxValue::Number(-n))
                         } else {
-                            Err(InterpretationError::new(self))
+                            Err(InterpretationError::OperandMustBeNumber(operator.clone()))
                         }
                     }
 
@@ -34,7 +40,7 @@ impl Interpretable for Expr {
                         LoxValue::Nil => Ok(LoxValue::Bool(true)),
                         _ => Ok(LoxValue::Bool(false)),
                     },
-                    _ => Err(InterpretationError::new(self)),
+                    op => Err(InterpretationError::InvalidUnaryOperator(op.clone())),
                 }
             }
             Expr::Binary {
@@ -50,7 +56,7 @@ impl Interpretable for Expr {
                         (LoxValue::Number(n1), LoxValue::Number(n2)) => {
                             Ok(LoxValue::Number(n1 - n2))
                         }
-                        _ => Err(InterpretationError::new(self)),
+                        _ => Err(InterpretationError::OperandMustBeNumber(operator.clone())),
                     },
 
                     TokenType::Plus => match (left_value, right_value) {
@@ -60,45 +66,47 @@ impl Interpretable for Expr {
                         (LoxValue::Str(s1), LoxValue::Str(s2)) => {
                             Ok(LoxValue::Str(format!("{}{}", s1, s2)))
                         }
-                        _ => Err(InterpretationError::new(self)),
+                        _ => Err(InterpretationError::OperandMustBeNumberOrString(
+                            operator.clone(),
+                        )),
                     },
 
                     TokenType::Slash => match (left_value, right_value) {
                         (LoxValue::Number(n1), LoxValue::Number(n2)) => {
                             Ok(LoxValue::Number(n1 / n2))
                         }
-                        _ => Err(InterpretationError::new(self)),
+                        _ => Err(InterpretationError::OperandMustBeNumber(operator.clone())),
                     },
 
                     TokenType::Star => match (left_value, right_value) {
                         (LoxValue::Number(n1), LoxValue::Number(n2)) => {
                             Ok(LoxValue::Number(n1 * n2))
                         }
-                        _ => Err(InterpretationError::new(self)),
+                        _ => Err(InterpretationError::OperandMustBeNumber(operator.clone())),
                     },
 
                     TokenType::Greater => match (left_value, right_value) {
                         (LoxValue::Number(n1), LoxValue::Number(n2)) => Ok(LoxValue::Bool(n1 > n2)),
-                        _ => Err(InterpretationError::new(self)),
+                        _ => Err(InterpretationError::OperandMustBeNumber(operator.clone())),
                     },
 
                     TokenType::GreaterEqual => match (left_value, right_value) {
                         (LoxValue::Number(n1), LoxValue::Number(n2)) => {
                             Ok(LoxValue::Bool(n1 >= n2))
                         }
-                        _ => Err(InterpretationError::new(self)),
+                        _ => Err(InterpretationError::OperandMustBeNumber(operator.clone())),
                     },
 
                     TokenType::Less => match (left_value, right_value) {
                         (LoxValue::Number(n1), LoxValue::Number(n2)) => Ok(LoxValue::Bool(n1 < n2)),
-                        _ => Err(InterpretationError::new(self)),
+                        _ => Err(InterpretationError::OperandMustBeNumber(operator.clone())),
                     },
 
                     TokenType::LessEqual => match (left_value, right_value) {
                         (LoxValue::Number(n1), LoxValue::Number(n2)) => {
                             Ok(LoxValue::Bool(n1 <= n2))
                         }
-                        _ => Err(InterpretationError::new(self)),
+                        _ => Err(InterpretationError::OperandMustBeNumber(operator.clone())),
                     },
 
                     TokenType::EqualEqual => match (left_value, right_value) {
@@ -120,26 +128,60 @@ impl Interpretable for Expr {
                         _ => Ok(LoxValue::Bool(true)),
                     },
 
-                    _ => Err(InterpretationError::new(self)),
+                    tt => Err(InterpretationError::InvalidBinaryOperator(tt.clone())),
                 }
             }
-            _ => Err(InterpretationError::new(self)),
+            e => Err(InterpretationError::InvalidExpression(e.clone())),
         }
     }
 }
 
-pub struct InterpretationError {
-    message: String,
+pub enum InterpretationError {
+    OperandMustBeNumber(Token),
+    OperandMustBeNumberOrString(Token),
+    InvalidLiteral(TokenType),
+    InvalidUnaryOperator(TokenType),
+    InvalidBinaryOperator(TokenType),
+    InvalidExpression(Expr),
 }
 
-impl InterpretationError {
-    fn new(expr: &Expr) -> Self {
-        InterpretationError {
-            message: format!("Unexpected token {:?}", expr),
+impl Display for InterpretationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            InterpretationError::OperandMustBeNumber(token) => {
+                write!(f, "Operand must be a number: {:?}", token)
+            }
+            InterpretationError::OperandMustBeNumberOrString(token) => {
+                write!(f, "Operand must be a number or string: {:?}", token)
+            }
+            InterpretationError::InvalidLiteral(token_type) => {
+                write!(f, "Invalid literal: {:?}", token_type)
+            }
+            InterpretationError::InvalidUnaryOperator(token_type) => {
+                write!(f, "Invalid unary operator: {:?}", token_type)
+            }
+            InterpretationError::InvalidBinaryOperator(token_type) => {
+                write!(f, "Invalid binary operator: {:?}", token_type)
+            }
+            InterpretationError::InvalidExpression(expr) => {
+                write!(f, "Invalid expression: {:?}", expr)
+            }
         }
     }
-
-    fn message(&self) -> &str {
-        &self.message
-    }
 }
+
+// pub struct InterpretationError {
+//     message: String,
+// }
+
+// impl InterpretationError {
+//     fn new(expr: &Expr) -> Self {
+//         InterpretationError {
+//             message: format!("Unexpected token {:?}", expr),
+//         }
+//     }
+
+//     fn message(&self) -> &str {
+//         &self.message
+//     }
+// }
